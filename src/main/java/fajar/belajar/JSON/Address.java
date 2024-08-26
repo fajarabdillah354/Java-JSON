dc227ba698c068e02b1b7abeaa8695ba9e54cdb7
unknown <fajardillah25@gmail.com>
2024-08-26 23:32:20 +0700

    Upload

---
+++ src/main/java/fajar/belajar/JSON/Address.java
@@ -0,0 +1,40 @@
+package fajar.belajar.JSON;
+
+import jakarta.validation.constraints.NotNull;
+
+public class Address {
+
+    @NotNull
+    private String city;
+
+
+    @NotNull
+    private String street;
+
+    @NotNull
+    private String country;
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+}
